3ac3c56a8057f1bb24b3d969a4b0f13cd986cd07
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-10-04 16:52:15 +0900

    authentication

--- app/(auth)/(routes)/register/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>Register page</div>;
-};
-
-export default page;
--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,9 @@
 import { ReactNode } from "react";
 
-const layout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+const AuthLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="flex items-center justify-center h-full">{children}</div>
+  );
 };
 
-export default layout;
+export default AuthLayout;
