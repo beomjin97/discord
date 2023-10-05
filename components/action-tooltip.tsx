8da4ec170b3b85847ef753d76fb336aa04875767
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-10-05 20:04:39 +0900

    create server modal

---
+++ components/action-tooltip.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import {
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+  Tooltip,
+} from "@/components/ui/tooltip";
+import { ReactNode } from "react";
+
+interface ActionTooltipProps {
+  label: string;
+  children: ReactNode;
+  side?: "top" | "right" | "bottom" | "left";
+  align?: "start" | "center" | "end";
+}
+
+export const ActionTooltip = ({
+  label,
+  children,
+  side,
+  align,
+}: ActionTooltipProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={50}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent side={side} align={align}>
+          <p className="text-sm font-semibold capitalize">
+            {label.toLowerCase()}
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
