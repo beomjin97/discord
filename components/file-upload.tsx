f1a9bab99a3209921e3b9f832537f2437ed89456
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-10-19 16:42:21 +0900

    message file attachment

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 import { UploadDropzone } from "@/lib/uploadthing";
 
@@ -21,6 +21,28 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
         <button
           onClick={() => onChange("")}
           className="absolute top-0 right-0 p-1 text-white rounded-full shadow-sm bg-rose-400"
+        >
+          <X className="w-4 h-4" />
+        </button>
+      </div>
+    );
+  }
+
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="w-10 h-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopender noreferror"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className="absolute p-1 text-white rounded-full shadow-sm -top-2 -right-2 bg-rose-400"
         >
           <X className="w-4 h-4" />
         </button>
