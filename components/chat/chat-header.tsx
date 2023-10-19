1cd1f107f8762381c4b15f031463572310585bbf
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-10-19 14:43:00 +0900

    socket io setup

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash } from "lucide-react";
 import { MobileToggle } from "../mobile-toggle";
 import { UserAvatar } from "../user-avatar";
+import { SocketIndicator } from "../socket-indicator";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -25,6 +26,9 @@ export const ChatHeader = ({
         <UserAvatar src={imageUrl} className="w-8 h-8 mr-2 md:h-8" />
       )}
       <p className="font-semibold text-black text-md dark:text-white">{name}</p>
+      <div className="flex items-center ml-auto">
+        <SocketIndicator />
+      </div>
     </div>
   );
 };
