ba292f60b9a1bbafee5271bd3d29252f88662b08
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-10-24 12:47:59 +0900

    form reset & close edit mode

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -73,8 +73,9 @@ export const ChatItem = ({
       });
 
       await axios.patch(url, values);
-      form.reset()
-      setIsEditing(false)
+
+      form.reset();
+      setIsEditing(false);
     } catch (error) {
       console.log(error);
     }
