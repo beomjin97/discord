b33409fc4954305acf06b7636510b248c5fab493
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-10-24 12:47:23 +0900

    chat item component

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -5,6 +5,11 @@ import { ChatWelcome } from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
+import { ChatItem } from "./chat-item";
+
+import { format } from "date-fns";
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
 interface ChatMessageProps {
   name: string;
@@ -69,7 +74,19 @@ export const ChatMessage = ({
         {data?.pages?.map((group, i) => (
           <Fragment key={i}>
             {group.items.map((message: MessageWithMemberWithProfile) => (
-              <div key={message.id}>{message.content}</div>
+              <ChatItem
+                key={message.id}
+                id={message.id}
+                currentMember={member}
+                member={message.member}
+                fileUrl={message.fileUrl}
+                content={message.content}
+                deleted={message.deleted}
+                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery}
+              />
             ))}
           </Fragment>
         ))}
